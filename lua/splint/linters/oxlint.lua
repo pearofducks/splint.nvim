b7fbef255ea7df0c2ab8b64d51e688156e808892
Dave Honneffer <pearofducks@gmail.com>
2026-04-13 00:06:47 +0200

    Fix oxlint config files

--- lua/splint/linters/oxlint.lua
+++ lua/splint/linters/oxlint.lua
@@ -9,10 +9,10 @@ local binary_name = "oxlint"
 
 return {
   cmd = function(ctx)
-    local local_binary = ctx.cwd .. '/node_modules/.bin/' .. binary_name
+    local local_binary = ctx.cwd .. "/node_modules/.bin/" .. binary_name
     return vim.loop.fs_stat(local_binary) and local_binary or binary_name
   end,
-  config_files = { "oxlintrc.json", ".oxlintrc.json" },
+  config_files = { ".oxlintrc.json", "oxlint.config.ts" },
   stdin = false,
   args = { "--format", "github" },
   stream = "stdout",
